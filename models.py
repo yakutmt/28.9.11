5f08633f72a67598e304548f755513f8f9461ecf
yakutmt <121279199+yakutmt@users.noreply.github.com>
2023-09-03 00:34:43 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+import typing as t
+from pydantic import BaseModel, Field
+
+class Pet(BaseModel):
+    name: str = Field(...)
+    animal_type: str = Field(...)
+    age: int = Field(...)
+    photo: str = Field(None)
+
+class PetsCollection(BaseModel):
+    pets: t.List[Pet] = Field(...)
+
+class Key(BaseModel):
+    key: str = Field(...)
